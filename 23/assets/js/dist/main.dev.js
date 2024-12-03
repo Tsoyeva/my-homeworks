55af2e74a3155820d081120e15ab7996133e70bb
Eve Tsoy <evetsoy@gmail.com>
2024-12-03 15:55:27 +0200

    added

--- 23/assets/js/dist/main.dev.js
+++ 23/assets/js/dist/main.dev.js
@@ -94,3 +94,80 @@ function findGCD() {
   var result = gcd(num1, num2);
   document.getElementById("result-gcd").innerText = "\u041D\u0430\u0438\u0431\u0456\u043B\u044C\u0448\u0438\u0439 \u0441\u043F\u0456\u043B\u044C\u043D\u0438\u0439\u0439 \u0434\u0456\u043B\u044C\u043D\u0438\u043A \u0447\u0438\u0441\u0435\u043B ".concat(num1, " \u0438 ").concat(num2, ": ").concat(result);
 }
+
+function checkPalindrome() {
+  var num = document.getElementById("number").value;
+
+  if (num.length !== 5 || isNaN(num)) {
+    document.getElementById("result-palindrome").innerText = "Будь ласка, введіть п’ятизначне число.";
+    return;
+  }
+
+  var firstDigit = num[0];
+  var secondDigit = num[1];
+  var fourthDigit = num[3];
+  var fifthDigit = num[4];
+
+  if (firstDigit === fifthDigit && secondDigit === fourthDigit) {
+    document.getElementById("result-palindrome").innerText = "\u0427\u0438\u0441\u043B\u043E ".concat(num, " \u0454 \u043F\u0430\u043B\u0456\u043D\u0434\u0440\u043E\u043C\u043E\u043C.");
+  } else {
+    document.getElementById("result-palindrome").innerText = "\u0427\u0438\u0441\u043B\u043E ".concat(num, " \u043D\u0435 \u0454 \u043F\u0430\u043B\u0456\u043D\u0434\u0440\u043E\u043C\u043E\u043C.");
+  }
+}
+
+function calculateDiscount() {
+  var purchaseAmount = parseFloat(document.getElementById("amount").value);
+
+  if (isNaN(purchaseAmount) || purchaseAmount <= 0) {
+    document.getElementById("result-discount").innerText = "Будь ласка, введіть коректну суму покупки.";
+    return;
+  }
+
+  var discount = 0;
+
+  if (purchaseAmount >= 200 && purchaseAmount < 300) {
+    discount = 3;
+  } else if (purchaseAmount >= 300 && purchaseAmount < 500) {
+    discount = 5;
+  } else if (purchaseAmount >= 500) {
+    discount = 7;
+  }
+
+  var discountAmount = purchaseAmount * discount / 100;
+  var totalAmount = purchaseAmount - discountAmount;
+  document.getElementById("result-discount").innerText = "\u0421\u0443\u043C\u0430 \u043F\u043E\u043A\u0443\u043F\u043A\u0438: ".concat(purchaseAmount.toFixed(2), " \u0433\u0440\u043D.\n        \u0417\u043D\u0438\u0436\u043A\u0430: ").concat(discount, "% (").concat(discountAmount.toFixed(2), " \u0433\u0440\u043D).\n        \u0421\u0443\u043C\u0430 \u0434\u043E \u043E\u043F\u043B\u0430\u0442\u0438: ").concat(totalAmount.toFixed(2), " \u0433\u0440\u043D.");
+}
+
+function analyzeNumbers() {
+  var positiveCount = 0;
+  var negativeCount = 0;
+  var zeroCount = 0;
+  var evenCount = 0;
+  var oddCount = 0;
+
+  for (var i = 1; i <= 10; i++) {
+    var userInput = parseFloat(prompt("\u0412\u0432\u0435\u0434\u0456\u0442\u044C \u0447\u0438\u0441\u043B\u043E ".concat(i, " \u0437 10:")));
+
+    if (isNaN(userInput)) {
+      alert("Введіть коректне число!");
+      i--;
+      continue;
+    }
+
+    if (userInput > 0) {
+      positiveCount++;
+    } else if (userInput < 0) {
+      negativeCount++;
+    } else {
+      zeroCount++;
+    }
+
+    if (userInput % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+  }
+
+  document.getElementById("result-stat").innerText = "\n                \u0421\u0442\u0430\u0442\u0438\u0441\u0442\u0438\u043A\u0430:\n                \u0414\u043E\u0434\u0430\u0442\u043D\u0456\u0445 \u0447\u0438\u0441\u0435\u043B: ".concat(positiveCount, "\n                \u0412\u0456\u0434'\u0454\u043C\u043D\u0438\u0445 \u0447\u0438\u0441\u0435\u043B: ").concat(negativeCount, "\n                \u041D\u0443\u043B\u0456\u0432: ").concat(zeroCount, "\n                \u041F\u0430\u0440\u043D\u0438\u0445 \u0447\u0438\u0441\u0435\u043B: ").concat(evenCount, "\n                \u041D\u0435\u043F\u0430\u0440\u043D\u0438\u0445 \u0447\u0438\u0441\u0435\u043B: ").concat(oddCount, "\n            ");
+}
--- 23/assets/js/main.js
+++ 23/assets/js/main.js
@@ -100,3 +100,94 @@ function findGCD() {
 
     document.getElementById("result-gcd").innerText = `Наибільший спільнийй дільник чисел ${num1} и ${num2}: ${result}`;
 }
+
+function checkPalindrome() {
+    const num = document.getElementById("number").value;
+
+    if (num.length !== 5 || isNaN(num)) {
+        document.getElementById("result-palindrome").innerText = 
+            "Будь ласка, введіть п’ятизначне число.";
+        return;
+    }
+
+    const firstDigit = num[0];
+    const secondDigit = num[1];
+    const fourthDigit = num[3];
+    const fifthDigit = num[4];
+
+    if (firstDigit === fifthDigit && secondDigit === fourthDigit) {
+        document.getElementById("result-palindrome").innerText = 
+            `Число ${num} є паліндромом.`;
+    } else {
+        document.getElementById("result-palindrome").innerText = 
+            `Число ${num} не є паліндромом.`;
+    }
+}
+
+function calculateDiscount() {
+    const purchaseAmount = parseFloat(document.getElementById("amount").value);
+
+    if (isNaN(purchaseAmount) || purchaseAmount <= 0) {
+        document.getElementById("result-discount").innerText = 
+            "Будь ласка, введіть коректну суму покупки.";
+        return;
+    }
+
+    let discount = 0;
+    if (purchaseAmount >= 200 && purchaseAmount < 300) {
+        discount = 3;
+    } else if (purchaseAmount >= 300 && purchaseAmount < 500) {
+        discount = 5;
+    } else if (purchaseAmount >= 500) {
+        discount = 7;
+    }
+
+    const discountAmount = (purchaseAmount * discount) / 100;
+    const totalAmount = purchaseAmount - discountAmount;
+
+    document.getElementById("result-discount").innerText = 
+        `Сума покупки: ${purchaseAmount.toFixed(2)} грн.
+        Знижка: ${discount}% (${discountAmount.toFixed(2)} грн).
+        Сума до оплати: ${totalAmount.toFixed(2)} грн.`;
+}
+
+function analyzeNumbers() {
+    let positiveCount = 0;
+            let negativeCount = 0;
+            let zeroCount = 0;
+            let evenCount = 0;
+            let oddCount = 0;
+
+            for (let i = 1; i <= 10; i++) {
+                const userInput = parseFloat(prompt(`Введіть число ${i} з 10:`));
+
+                if (isNaN(userInput)) {
+                    alert("Введіть коректне число!");
+                    i--; 
+                    continue;
+                }
+
+                if (userInput > 0) {
+                    positiveCount++;
+                } else if (userInput < 0) {
+                    negativeCount++;
+                } else {
+                    zeroCount++;
+                }
+
+                if (userInput % 2 === 0) {
+                    evenCount++;
+                } else {
+                    oddCount++;
+                }
+            }
+
+            document.getElementById("result-stat").innerText = `
+                Статистика:
+                Додатніх чисел: ${positiveCount}
+                Від'ємних чисел: ${negativeCount}
+                Нулів: ${zeroCount}
+                Парних чисел: ${evenCount}
+                Непарних чисел: ${oddCount}
+            `;
+}
